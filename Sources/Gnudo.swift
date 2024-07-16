24eb486d9d0d7c3907dd3cd8575d486cd1e8f8ab
Mana Walsh <mana.matin.walsh@gmail.com>
2024-07-16 07:32:18 +0100

    Correctly creates ~/.gnudodata.json

--- Sources/Gnudo.swift
+++ Sources/Gnudo.swift
@@ -9,14 +9,21 @@ import ArgumentParser
 
 @main struct Gnudo: ParsableCommand {
     static let configuration = CommandConfiguration(abstract: "A To-Do list app with a name pertaining to wildebeest.", version: "v0.0.1")
-    static let dataPath: String = "~/.gnudodata.json"
    
     mutating func run() throws  {
         let fm = FileManager()
-        if (!fm.fileExists(atPath: Gnudo.dataPath)) {
+        let dataPath: String = String("\(fm.homeDirectoryForCurrentUser).gnudodata.json".dropFirst(7))
+        let maxDataFileCreationTries:Int = 3
+       
+        var fileCreationLoopCount: Int = 0
+        while (!fm.fileExists(atPath: dataPath) && fileCreationLoopCount < maxDataFileCreationTries) {
             print("There is no data file at the expected loacation.")
-            print("Creating \(Gnudo.dataPath)")
-            fm.createFile(atPath: Gnudo.dataPath, contents: nil)
+            print("Creating \(dataPath)...")
+            fm.createFile(atPath: "",contents: nil)
+            fileCreationLoopCount += 1
         }
-   }
+        if (!fm.fileExists(atPath: dataPath)) {
+            fatalError("Failed to create \(dataPath) after \(maxDataFileCreationTries) attempts. Aborting execution.")
+        } else { print("Successfully created datafile.")}
+    }
 }
