82627c41e39c44ebdfad64ac7ba40907cd90d94e
Mana Walsh <mana.matin.walsh@gmail.com>
2024-07-16 07:10:40 +0100

    Change Package.swift: Only Support macOs 10.13+

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "gnudo",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     ],
