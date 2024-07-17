d32e0aa089b3ec5d37a1573ee91aa42178efcab2
Mana Walsh <mana.matin.walsh@gmail.com>
2024-07-17 17:43:46 +0100

    REN ensureGnudoJson() & now creates w/ todo list

--- Sources/Gnudo.swift
+++ Sources/Gnudo.swift
@@ -1,33 +1,29 @@
-//
-// main.swift
-//
-// Created by Mana Walsh on 15/07/2024.
-//
-
 import Foundation
 import ArgumentParser
 
 @main struct Gnudo: ParsableCommand {
     static let configuration = CommandConfiguration(abstract: "A To-Do list app with a name pertaining to wildebeest.", version: "v0.0.1")
-   
     mutating func run() throws  {
         let fm = FileManager()
+        let jsonifier = JSONEncoder()
+        jsonifier.outputFormatting = .prettyPrinted
         let dataPath: String = String("\(fm.homeDirectoryForCurrentUser).gnudodata.json".dropFirst(7))
-        ensureGnudoJsonfile(atPath: dataPath, withMaxAttemps: 3, usingFileManager: fm)
-       
+        prepGnudoJson(atPath: dataPath, usingFileManager: fm, encoder: jsonifier, andDefaultList: ToDoList(name: "Gnudo List", tasks: []))
     }
 }
 
-func ensureGnudoJsonfile(atPath path: String, withMaxAttemps maxAttempts: Int, usingFileManager fm: FileManager) {
-    var fileCreationLoopCount: Int = 0
-        while (!fm.fileExists(atPath: path) && fileCreationLoopCount < maxAttempts) {
-            print("There is no data file at the expected loacation.")
-            print("Creating \(path)...")
-            fm.createFile(atPath: "",contents: nil)
-            fileCreationLoopCount += 1
-        }
-        if (!fm.fileExists(atPath: path)) {
-            fatalError("Failed to create \(path) after \(maxAttempts) attempts. Aborting execution.")
-        } else { print("Successfully created datafile.")}
-
+func prepGnudoJson(atPath path: String, usingFileManager fm: FileManager, maxAttempts: Int = 3, encoder: JSONEncoder, andDefaultList list: ToDoList) {
+    if (fm.fileExists(atPath: path)) { return }
+    
+    let listData = try! encoder.encode(list)
+    
+    var fileCreationAttemps = 1
+    while (!fm.fileExists(atPath: path) && fileCreationAttemps <= maxAttempts) {
+        print("There is not a data file at the expected location.")
+        print("Creating \(path)... (Attempt \(fileCreationAttemps))")
+        fm.createFile(atPath: path, contents: listData)
+        fileCreationAttemps += 1
+    }
+    if (fm.fileExists(atPath: path)) { print((fileCreationAttemps == 1) ? "Successfully created data file!" : "Created the datafile! (🎉!First Try!🎉)"); return }
+    fatalError("Failed to create data file at '\(path)'. Abort.")
 }
